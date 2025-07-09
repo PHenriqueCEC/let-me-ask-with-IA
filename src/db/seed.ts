2e9afbdf73c0d3c0b608f0a2c9f880bf96394d20
Pedro <pedroalmeidavrb@gmail.com>
2025-07-09 09:49:14 -0300

    Configurando backend

---
+++ src/db/seed.ts
@@ -0,0 +1,25 @@
+import { reset, seed } from "drizzle-seed";
+import { db, sql } from "./connection.ts";
+import { schema } from "./schema/index.ts";
+// biome-ignore lint/correctness/noUnusedImports: <explanation>
+import { rooms } from "./schema/rooms.ts";
+
+await reset(db, schema);
+
+await seed(db, schema).refine(f => {
+    return {
+        rooms: {
+            count: 20,
+            columns: {
+                name: f.companyName(),
+                description: f.loremIpsum(),
+            }
+        }
+    }
+});
+
+
+await sql.end();
+
+// biome-ignore lint/suspicious/noConsole: <explanation>
+console.log("Database reset and seeded successfully")
